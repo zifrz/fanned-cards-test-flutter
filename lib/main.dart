bd9fc9881432c73e9e101afb4f0f009c92904d56
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-01-25 19:09:29 +0000

    renamed appname and bundleid

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/home_page.dart';
+import 'package:fannedcardstestflutter/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/components/fanned_cards.dart';
+import 'package:fannedcardstestflutter/components/fanned_cards.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
